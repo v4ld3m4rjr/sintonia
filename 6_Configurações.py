179a7865d6627ed966c77e6854d7f591d35fa04d
v4ld3m4rjr <128973741+v4ld3m4rjr@users.noreply.github.com>
2025-05-23 15:36:37 -0300

    Create 6_Configurações.py

---
+++ 6_Configurações.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def configuracoes_page():
+    st.title("Configurações do Usuário")
+    st.subheader("Perfil")
+    # Dados pessoais aqui
+
+    st.subheader("Preferências")
+    # UI e notificações aqui
+
+    st.subheader("Metas e Objetivos")
+    # Definição e acompanhamento aqui
+
+    st.subheader("Dados e Privacidade")
+    # Gestão de informações aqui
+
+if __name__ == "__main__":
+    configuracoes_page()
