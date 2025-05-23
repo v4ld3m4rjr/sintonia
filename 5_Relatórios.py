0982fe67b45d62c9c9fb1f70599c99b42e5a5fc1
v4ld3m4rjr <128973741+v4ld3m4rjr@users.noreply.github.com>
2025-05-23 15:35:39 -0300

    Create 5_Relatórios.py

---
+++ 5_Relatórios.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def relatorios_page():
+    st.title("Geração de Relatórios")
+    st.subheader("Relatórios Predefinidos")
+    # Modelos prontos aqui
+
+    st.subheader("Exportação")
+    # PDF, Excel, CSV aqui
+
+    st.subheader("Compartilhamento")
+    # Links temporários aqui
+
+if __name__ == "__main__":
+    relatorios_page()
